8a481ed22fc574c4e24b6d84653d53e3f56b8dc6
Muedgar <edgarmutangana@gmail.com>
2023-01-01 23:48:58 +0200

    upload project

---
+++ app.js
@@ -0,0 +1,7 @@
+function handleMouseOver() {
+    document.querySelector(".article-links").style.display = "flex";
+}
+
+function handleMouseOut() {
+    document.querySelector(".article-links").style.display = "none";
+}
